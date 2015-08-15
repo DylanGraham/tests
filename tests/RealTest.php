ffb4ea502024b5d25750ca3d6988807945be3a9c
Dylan Graham <dylan.graham@gmail.com>
2015-08-15 17:37:31 +1000

    Initial commit

---
+++ tests/RealTest.php
@@ -0,0 +1,12 @@
+<?php
+
+class RealTest extends TestCase
+{
+    public function test_main_page()
+    {
+        $this->visit('/')
+            ->see('SeqTrack')
+            ->click('Enter samples')
+            ->seePageIs('/samples');
+    }
+}
