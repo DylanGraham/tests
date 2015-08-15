87ab85f1910dd49716005894854999b55f69c23a
Dylan Graham <dylan.graham@gmail.com>
2015-08-15 22:59:42 +1000

    Initial sample form

--- resources/views/samples/create.blade.php
+++ resources/views/samples/create.blade.php
@@ -3,4 +3,13 @@
     <h1>SeqTrack - Enter samples</h1>
     <hr/>
 
+    {!! Form::open(['class' => 'form-inline']) !!}
+        <div class="form-inline">
+        {!! Form::label('basc-project', 'BASC Project', ['class' => 'sr-only']) !!}
+        {!! Form::text('basc-project', null, ['class' => 'form-control', 'placeholder' => 'BASC Project']) !!}
+        {!! Form::label('name', 'Name', ['class' => 'sr-only']) !!}
+        {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+
+        </div>
+    {!! Form::close() !!}
 @endsection
